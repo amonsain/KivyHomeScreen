20ee7a06dbb92cab27268eb43ad37bb0cc10e163
amonsain <augustin@macbook-pro.home>
2016-01-19 22:09:15 +0100

    GmapAPI update

--- Traffi.py
+++ Traffi.py
@@ -5,6 +5,7 @@ GMAPIkey = 'AIzaSyAC24-Uuz1Fo04Q3J6gjrJU_v86PPEYFJ0'
 
 def get_transittime(start,destination):
 
+https://maps.googleapis.com/maps/api/directions/json?origin=Colomiers&destination=Lacroix-Falgarde&mode=driving&traffic_model=optimistic&departure_time=now
 
 	retour_api_meteo = urllib.urlopen(url)
 	Json_string = retour_api_meteo.read().decode('utf-8')
